41c395f6d53796987dee94bbc02375604f13c5d1
Bilal <99763904+bial9@users.noreply.github.com>
2022-04-21 04:13:10 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,49 @@
+// var firstName = prompt("Enter first name");
+// var lastName = prompt("Enter last name");
+// var fullName = firstName + " " +lastName;
+// document.write(fullName);
+
+// var mobile = prompt("Your Favourite mobile");
+// document.write(mobile+"<br>")
+// document.write("length of string: "+mobile.length)
+
+// var a = "pakistani";
+// document.write(a+"<br>")
+// document.write(a.indexOf("n"))
+
+// var a = "Hello World";
+// document.write(a+"<br>")
+// document.write(a.lastIndexOf("l"))
+
+
+// var a = "Pakistani";
+// document.write(a+"<br>")
+// var b = a.charAt(3);
+// document.write(b)
+
+// var firstName = prompt("Enter first name");
+// var lastName = prompt("Enter last name");
+// var fullName = firstName.concat(" ",lastName)
+// document.write(fullName);
+
+// var a = "Hyderabad"
+// document.write(a)
+// var b = a.replace("Hyderabad","Islamabad")
+// document.write(b)
+
+// var message = "Ali and Sami are best friends. They play cricket and football together.";
+// document.write(message.replace(/and/g,"&"))
+
+// var a = prompt("enter")
+// document.write(a.toUpperCase())
+
+// var a = prompt("enter");
+// var b = a[0].toUpperCase() + a.slice(1)
+// document.write(b)
+
+// var a = 30.10
+// var b = a.toString()
+// document.write(b)
+
+// var university = "University of Karachi";
+// document.write(university.split(0))
